3d20ef1c2a01ede6f47f98abda5bfdbd018184de
wweverma1 <wweverma1@gmail.com>
2024-02-04 11:41:01 +0530

    minor cors changes

--- app/utils/app_functions.py
+++ app/utils/app_functions.py
@@ -27,6 +27,8 @@ def after_request(response):
     if request.endpoint:
         end_time = time.time()
         latency = int((end_time - request.start_time) * 1000)
+        header = response.headers
+        header['Access-Control-Allow-Origin'] = '*'
         print(f'[ {str(datetime.now())} ] endpoint {request.endpoint} latency {latency} req_id {request.environ.get("FLASK_REQUEST_ID")}')
     return response
 
