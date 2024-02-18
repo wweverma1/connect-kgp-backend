ffa748b0f8b818b84d6cacfade1fdfdc6bd9bd08
wweverma1 <wweverma1@gmail.com>
2024-02-18 06:09:32 +0530

    bugfixes

--- app/home/models.py
+++ app/home/models.py
@@ -17,18 +17,26 @@ class Rating(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.now())
     rating = db.Column(db.Integer, default=0)
     rated_by_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    
+
     @staticmethod
     def post_rating(user_id, rating):
         try:
-            rating = Rating(
-                created_at=datetime.now(),
-                rating=rating,
-                rated_by_user_id=user_id
-            )
-            db.session.add(rating)
-            db.session.commit()
-            return rating
+            existing_rating = Rating.query.filter_by(rated_by_user_id=user_id).first()
+
+            if existing_rating:
+                existing_rating.created_at = datetime.now()
+                existing_rating.rating = rating
+                db.session.commit()
+                return existing_rating
+            else:
+                new_rating = Rating(
+                    created_at=datetime.now(),
+                    rating=rating,
+                    rated_by_user_id=user_id
+                )
+                db.session.add(new_rating)
+                db.session.commit()
+                return new_rating
         except SQLAlchemyError as e:
             print(e)
             traceback.print_exc()
