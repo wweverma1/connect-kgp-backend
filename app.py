a2163bda154a2c9c9815b079d9cfd8f35b1f31b0
wweverma1 <wweverma1@gmail.com>
2024-02-27 19:21:26 +0530

    changed the matchmaking algo

--- app.py
+++ app.py
@@ -5,147 +5,85 @@ from app.user.models import User
 from app import db
 import traceback
 from sqlalchemy.exc import SQLAlchemyError
+import random
 
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-idleUsers = set()
-idleAudioUsers = set()
-matches = {}
-userInfo = {}
+users = {}
+available_users = {'text': set(), 'voice': set()}
 
-class UserData:
-    def __init__(self, chat_type, uid):
-        self.chat_type = chat_type
-        self.uid = uid
+def matchmake(user_sid, chatType):
+    available_users[chatType].add(user_sid)
 
-def handle_idle(user_id, chatType):
-    global idleUsers
-    global idleAudioUsers
-    global matches
+    if len(available_users[chatType] >= 2):
+        match_sid = random.choice(list(available_users[chatType] - {user_sid}))
 
-    if chatType == "voice":
-        idleAudioUsers.add(user_id)
-        if len(idleAudioUsers) > 1:
-            matched_user_1 = idleAudioUsers.pop()
-            matched_user_2 = idleAudioUsers.pop()
-            if matched_user_1 != matched_user_2:
-                print("** match **\t", matched_user_1, " - ", matched_user_2)
-                matches[matched_user_1] = matched_user_2
-                matches[matched_user_2] = matched_user_1
-                socketio.emit('match', {'user_id': matched_user_2}, to=matched_user_1)
-    else:
-        idleUsers.add(user_id)
-        if len(idleUsers) > 1:
-            matched_user_1 = idleUsers.pop()
-            matched_user_2 = idleUsers.pop()
-            if matched_user_1 != matched_user_2:
-                print("** match **\t", matched_user_1, " - ", matched_user_2)
-                matches[matched_user_1] = matched_user_2
-                matches[matched_user_2] = matched_user_1
-                socketio.emit('match', {'user_id': matched_user_2}, to=matched_user_1)
-                socketio.emit('match', {'user_id': matched_user_1}, to=matched_user_2)
+        users[user_sid]['match_sid'] = match_sid
+        users[match_sid]['match_sid'] = user_sid
+
+        if chatType == "text":
+            socketio.emit('match', {'user_id': match_sid}, to=user_sid)
+            socketio.emit('match', {'user_id': user_sid}, to=match_sid)
+        else:
+            if random.choice([user_sid, match_sid]) == user_sid:
+                socketio.emit('match', {'user_id': match_sid}, to=user_sid)
+            else:
+                socketio.emit('match', {'user_id': user_sid}, to=match_sid)
+
+        available_users[chatType].remove(user_sid)
+        available_users[chatType].remove(match_sid)
 
 @socketio.on('connection')
 def handle_connection(data):
-    global idleUsers
-    global idleAudioUsers
-    global matches
-    global userInfo
+    user_sid = request.sid
+    chatType = data.get('chat_type')
+    user_id = data.get('uid')
 
-    user_id = request.sid
+    if user_id is not None:
+        print("** user joined **\t", user_sid, chatType, user_id)
 
-    chatType = data['chat-type']
-
-    print("** user joined **\t", user_id, chatType, data['uid'])
-
-    value_user_data = UserData(chatType, data['uid'])
-    userInfo[user_id] = value_user_data
-    
-    if chatType == "voice":
-        if idleAudioUsers:
-            match_id = idleAudioUsers.pop()
-            if user_id != match_id:
-                print("** match **\t", user_id, " - ", match_id)
-                matches[user_id] = match_id
-                matches[match_id] = user_id
-                socketio.emit('match', {'user_id': match_id}, to=user_id)
-        else:
-            handle_idle(user_id, chatType)
-    else:
-        if idleUsers:
-            match_id = idleUsers.pop()
-            if user_id != match_id:
-                print("** match **\t", user_id, " - ", match_id)
-                matches[user_id] = match_id
-                matches[match_id] = user_id
-                socketio.emit('match', {'user_id': match_id}, to=user_id)
-                socketio.emit('match', {'user_id': user_id}, to=match_id)
-        else:
-            handle_idle(user_id, chatType)
+        users[user_sid] = {
+            'user_id': user_id,
+            'chat_type': chatType,
+            'match_sid': None
+        }
+        matchmake(user_sid, chatType)
 
 @socketio.on('callUser')
 def handle_call_user(signal):
-    global matches 
-    
-    match_id = matches[request.sid]
-    socketio.emit('callUser', { 'signal': signal, 'match_id': match_id }, to=matches[request.sid])
+    match_sid = users[request.sid]['match_sid']
+    socketio.emit('callUser', { 'signal': signal, 'match_id': request.sid }, to=match_sid)
 
 @socketio.on('answerCall')
 def handle_answer_call(signal):
-    global matches
-    
-    socketio.emit('callAccepted', signal, to=matches[request.sid])
+    socketio.emit('callAccepted', signal, to=users[request.sid]['match_sid'])
 
 @socketio.on('message')
 def handle_chat_message(message):
-    global idleUsers
-    global matches
-
-    socketio.emit('message', message, to=matches[request.sid])
+    socketio.emit('message', message, to=users[request.sid]['match_sid'])
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    global idleUsers
-    global matches
-    global idleAudioUsers
-    global userInfo
-
-    user_id = request.sid
-    user_data = userInfo[user_id]
-    chatType = user_data.chat_type
-    del userInfo[user_id]
-
-    print("** user left **\t", user_id, chatType)
-
-    if chatType == "voice":
-        if user_id in idleAudioUsers:
-            idleAudioUsers.remove(user_id)
-        else:
-            match_id = matches[user_id]
-            socketio.emit('unmatch', to=match_id)
-            del matches[user_id]
-            del matches[match_id]
-            handle_idle(match_id, chatType)
-    elif chatType == "text":
-        if user_id in idleUsers:
-            idleUsers.remove(user_id)
-        else:
-            match_id = matches[user_id]
-            socketio.emit('unmatch', to=match_id)
-            del matches[user_id]
-            del matches[match_id]
-            handle_idle(match_id, chatType)
+    user_sid = request.sid
+    chatType = users[user_sid]['chat_type']
+    match_sid = users[user_sid]['match_sid']
+    if match_sid is not None:
+        print("** user left **\t", user_sid, chatType, "not-idle")
+        users[match_sid]['match_sid'] = None
+        socketio.emit('unmatch', to=match_sid)
+        matchmake(match_sid, chatType)
+    else:
+        print("** user left **\t", user_sid, chatType, "idle")
+    del users[user_sid]
 
 @socketio.on('report')
 def handle_report():
-    global matches
-    global userInfo
+    user_sid = request.sid
+    match_sid = users[user_sid]['match_sid']
+    match_uid = users[match_sid]['user_id']
 
-    user_id = request.sid
-    match_id = matches[user_id]
-    report_user_data = userInfo[match_id]
-
-    user = db.session.query(User).filter_by(id=report_user_data.uid).one_or_none()
+    print("** reporting user **\t", match_uid)
+    user = db.session.query(User).filter_by(id=match_uid).one_or_none()
     if user:
         try:
             user.rating -= 1
@@ -154,21 +92,19 @@ def handle_report():
             print(e)
             traceback.print_exc()
             db.session.rollback()
-
     
 @socketio.on('shuffleCall')
 def shuffleCall():
-    global idleUsers
-    global matches
-    global idleAudioUsers
-
-    user_id = request.sid
-    match_id = matches[user_id]
-    socketio.emit('unmatch', to=match_id)
-    del matches[user_id]
-    del matches[match_id]
-    handle_idle(match_id, 'voice')
-    handle_idle(user_id, 'voice')
+    user_sid = request.sid
+    match_sid = users[user_sid]['match_sid']
+    
+    socketio.emit('unmatch', to=match_sid)
+    
+    users[user_sid]['match_sid'] = None
+    users[match_sid]['match_sid'] = None
+    
+    matchmake(user_sid, "voice")
+    matchmake(match_sid, "voice")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
