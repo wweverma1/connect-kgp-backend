bb973a1a7e369d0d95a0689d4a0bfbc9a4506f2f
wweverma1 <wweverma1@gmail.com>
2024-03-02 21:31:20 +0530

    bugfix

--- app/user/controller.py
+++ app/user/controller.py
@@ -247,24 +247,31 @@ def getFriends():
         return jsonify({'error': str(e)}), 500
     
 def removeFriend():
-    uid = request.form['user_id']
-    fid = request.form['friend_id']
+    try:
+        uid = int(request.form['user_id'])
+        fid = int(request.form['friend_id'])
 
-    user = db.session.query(User).filter_by(id=uid).one_or_none()
-    friend = db.session.query(User).filter_by(id=fid).one_or_none()
-    if user and friend:
-        try:
-            user.friends.remove(fid)
-            friend.friends.remove(uid)
-            db.session.commit()
-            return jsonify({"message": "friend removed"}), 200
-        except SQLAlchemyError as e:
-            print(e)
-            traceback.print_exc()
-            db.session.rollback()
-            return jsonify({"error": "couldn't remove friend"}), 500
-    else:
-        return jsonify({"error": "user/ users not found"}), 400
+        user = db.session.query(User).filter_by(id=uid).one_or_none()
+        friend = db.session.query(User).filter_by(id=fid).one_or_none()
+
+        if user and friend:
+            try:
+                user.friends.remove(fid)
+                friend.friends.remove(uid)
+                db.session.commit()
+                return jsonify({"message": "Friend removed"}), 200
+            except SQLAlchemyError as e:
+                print(e)
+                traceback.print_exc()
+                db.session.rollback()
+                return jsonify({"error": "Couldn't remove friend"}), 500
+        else:
+            return jsonify({"error": "User/users not found"}), 400
+
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
+        return jsonify({"error": "Invalid input"}), 400
 
 
 
