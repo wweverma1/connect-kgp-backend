c01570ae5a16cc9b8e49a217292f8bee94ca1ddd
wweverma1 <wweverma1@gmail.com>
2024-03-22 20:36:07 +0530

    updated routes for admin apis

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -7,5 +7,5 @@ from app.admin.controller import (
 
 admin_api = Blueprint('admin', __name__)
 
-admin_api.add_url_rule(rule='/get-info', view_func=getInfo, methods=['GET'])
-admin_api.add_url_rule(rule='/inactivity-alert', view_func=sendInactivityAlerts, methods=['GET'])
+admin_api.add_url_rule(rule='/admin/get-info', view_func=getInfo, methods=['GET'])
+admin_api.add_url_rule(rule='/admin/inactivity-alert', view_func=sendInactivityAlerts, methods=['GET'])
