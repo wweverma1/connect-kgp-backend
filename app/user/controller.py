c0e25905fd52346d5bdec1854fbf60f0be4c3507
wweverma1 <wweverma1@gmail.com>
2024-04-01 19:45:30 +0530

    bugfix

--- app/user/controller.py
+++ app/user/controller.py
@@ -132,9 +132,10 @@ def postFeed():
     content = request.form['content'].strip()
     icon = request.form['icon']
     parent_feed_id = request.form.get('parent_feed_id')
-    imageFile = request.form.get('imageFile', None)
+    imageFile = request.form.get('imageFile')
 
-    if imageFile is not None:
+    if imageFile:
+        print("receieved image- ", imageFile)
         content+= f'<image>{imageFile}</image>'
 
     feed = Feed.post_feed(user_id, content, icon, parent_feed_id)
